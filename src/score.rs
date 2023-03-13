40e5d4bb9b44c2ab5196e19b8e3e9a5394139c6e
IdanArye <idanarye@gmail.com>
2023-03-13 02:40:39 +0200

    Report living opponents in score

--- src/score.rs
+++ src/score.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts};
 
+use crate::killing::Killable;
+
 pub struct ScorePlugin;
 
 impl Plugin for ScorePlugin {
@@ -24,13 +26,35 @@ impl ScoreHaver {
     }
 }
 
-fn show_score(mut egui_context: EguiContexts, score_havers: Query<&ScoreHaver>) {
+fn show_score(
+    mut egui_context: EguiContexts,
+    score_havers: Query<&ScoreHaver>,
+    opponents_query: Query<&Killable, Without<ScoreHaver>>,
+) {
+    let mut num_opponents = 0;
+    let mut opponents_alive = 0;
+    for killable in opponents_query.iter() {
+        num_opponents += 1;
+        if !killable.killed {
+            opponents_alive += 1;
+        }
+    }
     let panel = egui::Area::new("score-area").fixed_pos([0.0, 0.0]);
     panel.show(egui_context.ctx_mut(), |ui| {
+        ui.label(
+            egui::RichText::new(format!(
+                "Remaining: {} / {}",
+                opponents_alive, num_opponents
+            ))
+            .strong(),
+        );
         for score_haver in score_havers.iter() {
             ui.label(
-                egui::RichText::new(format!("{}: {}", score_haver.name, score_haver.score))
-                    .strong(),
+                egui::RichText::new(format!(
+                    "Killed by {}: {}",
+                    score_haver.name, score_haver.score
+                ))
+                .strong(),
             );
         }
     });
